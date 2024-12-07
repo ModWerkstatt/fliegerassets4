cd8b0dc4cec0c35a2bb39ce1794a43559d5d2287
Florian Thiel <flth@flth.de>
2024-12-07 15:57:51 +0100

    Import Bandion

---
+++ mod.lua
@@ -0,0 +1,23 @@
+function data()
+return {
+info = {
+  minorVersion = 0,
+  severityAdd = "NONE",
+  severityRemove = "CRITICAL",
+  name = _(info_name),
+  description = _(info_desc),
+  authors = {
+        {
+            name = "Bandion",
+            role = "CREATOR",
+            text = "Set Creator",
+            steamProfile = "",
+            tfnetId = 21221,
+        },
+  },
+  tags = {"europe", "brush asset", },
+  		visible = true,
+},
+
+ }
+end
---
+++ strings.lua
@@ -0,0 +1,16 @@
+function data()
+	info_name = "Flugzeug Asset 4"
+	info_desc = (
+				"Flugzeugset zum gestallten von Flugplatzscenen.")
+	return {
+		de = {
+			[info_name] = info_name,
+			[info_desc] = info_desc,
+		},
+		en = {
+			[info_name] = 	"Aircraft Asset 4",
+			[info_desc] = 	(
+				"Aircraft set to setup Airfieldscenes.")
+		},
+	}
+end
